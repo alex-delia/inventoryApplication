70532875dee624b73edcecd975160d36438e3cf1
alex-delia <adelia3@uwo.ca>
2024-06-18 15:35:42 -0400

    add prettier to project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  { rules: { "no-unused-vars": "warn", } }
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    {
+        rules: { 'no-unused-vars': 'warn' },
+        extends: ['prettier'],
+    },
 ];
