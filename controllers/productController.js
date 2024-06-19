16cb1f04baa5d22db386ccd5ed107aa704946dca
alex-delia <adelia3@uwo.ca>
2024-06-19 17:59:49 -0400

    handle validation for product create POST request

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,7 @@ const Brand = require('../models/brandSchema');
 const Category = require('../models/categorySchema');
 
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require("express-validator");
 
 //display home page
 exports.index = asyncHandler(async (req, res, next) => {
@@ -58,10 +59,68 @@ exports.product_create_get = asyncHandler(async (req, res, next) => {
 });
 
 //handle Product create on POST
-exports.product_create_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Product Create POST');
-});
+exports.product_create_post = [
+    body('name', 'Name must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('description', 'Description must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('price')
+        .trim()
+        .notEmpty()
+        .withMessage('Price must not be empty.')
+        .isNumeric()
+        .withMessage('Price must be numeric.')
+        .escape(),
+    body('salePercentage', 'Sale Percentage must be an integer.')
+        .optional({ values: "falsy" })
+        .trim()
+        .isInt()
+        .escape(),
+    body('inStock')
+        .trim()
+        .notEmpty()
+        .withMessage('Price must not be empty.')
+        .isInt()
+        .withMessage('Stock must be an integer.')
+        .escape(),
+    body('brand', 'Brand must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('cateogry.*')
+        .escape(),
 
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        const product = new Product({
+            name: req.body.name,
+            description: req.body.description
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('category_form', {
+                title: 'New Category',
+                category: category,
+                errors: errors.array()
+            });
+        } else {
+            // Data from form is valid.
+
+            // Save category.
+            await category.save();
+            // Redirect to new category record.
+            res.redirect(category.url);
+        }
+    })
+];
 //display Product delete form on GET
 exports.product_delete_get = asyncHandler(async (req, res, next) => {
     res.send('NOT IMPLEMENTED: Product Delete GET');
