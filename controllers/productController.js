3e64e1183618131aa8bfd23fe0dedda2f5a025c1
alex-delia <adelia3@uwo.ca>
2024-06-19 20:42:39 -0400

    implement POST request for update product

--- controllers/productController.js
+++ controllers/productController.js
@@ -180,6 +180,92 @@ exports.product_update_get = asyncHandler(async (req, res, next) => {
 });
 
 //handle Product update on POST
-exports.product_update_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Product Update POST');
-});
+exports.product_update_post = [
+    // Convert the categories to an array.
+    (req, res, next) => {
+        if (!Array.isArray(req.body.category)) {
+            req.body.category = typeof req.body.category === "undefined" ? [] : [req.body.category];
+        }
+        next();
+    },
+
+    body('name', 'Name must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('description', 'Description must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('price')
+        .trim()
+        .notEmpty()
+        .withMessage('Price must not be empty.')
+        .isNumeric()
+        .withMessage('Price must be numeric.')
+        .escape(),
+    body('salePercentage', 'Sale Percentage must be an integer.')
+        .optional({ values: "falsy" })
+        .trim()
+        .isInt()
+        .escape(),
+    body('inStock')
+        .trim()
+        .notEmpty()
+        .withMessage('Price must not be empty.')
+        .isInt()
+        .withMessage('Stock must be an integer.')
+        .escape(),
+    body('brand', 'Brand must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('category.*')
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        const product = new Product({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            salePercentage: req.body.salePercentage,
+            inStock: req.body.inStock,
+            brand: req.body.brand,
+            category: req.body.category,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            const [allBrands, allCategories] = await Promise.all([
+                Brand.find().sort({ name: 1 }).exec(),
+                Category.find().sort({ name: 1 }).exec()
+            ]);
+
+            // Mark our selected genres as checked.
+            allCategories.forEach(category => {
+                if (product.category.includes(category._id)) {
+                    category.checked = "true";
+                }
+            });
+
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('product_form', {
+                title: 'Update Product',
+                product: product,
+                brands: allBrands,
+                categories: allCategories,
+                errors: errors.array()
+            });
+        } else {
+            // Data from form is valid.
+            // Save product.
+            await Product.findByIdAndUpdate(req.params.id, product, {});
+            // Redirect to new product record.
+            res.redirect(product.url);
+        }
+    })
+];
